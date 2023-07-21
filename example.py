9a0c00bbeb1e34704d0bfbe80df58ca047021a38
canxin <1969730106@qq.com>
2023-07-22 00:13:32 +0800

    udpate

--- example.py
+++ example.py
@@ -19,21 +19,30 @@ async def main():
         print(message, end="")
     """ask a question and get answer with onlt http(no suggestions)"""
     answer = await poe_client.ask(url_botname="Assistant", question="Introduce openai", plain=True)
+
     """send chat break(claer the bot's memory)"""
     await poe_client.send_chat_break(url_botname="Assistant")
+
     """create a bot"""
     await poe_client.create_bot(handle="canxintest01", prompt="a ai assistant")
+
     """edit a bot"""
     await poe_client.edit_bot(url_botname="canxintest01", prompt="a computer programmer")
+
     """delete some conservation messages with specific bot"""
     await poe_client.delete_bot_conversation(url_botname="Assistant", count=2)
+
     """delete all conservation messages with specific bot"""
     await poe_client.delete_bot_conversation(url_botname="Assistant", del_all=True)
+
     """delete all conservation messages with all bots"""
     await poe_client.delete_all_conversations()
+
     """get some conversation messages with specific bot"""
-    messages = await poe_client.get_message_history(url_botname="Assistant",count=20)
+    messages = await poe_client.get_message_history(url_botname="Assistant", count=20)
+
     """get all conversation messages with specific bot"""
-    messages = await poe_client.get_message_history(url_botname="Assistant",count=20)
+    messages = await poe_client.get_message_history(url_botname="Assistant", count=20)
+
     """get bot configuration"""
     config = await poe_client.get_bot_configuration(url_botname="canxintest01")
